15d5777714841a7712fbca9af4fc2f45b5933be5
123GitHub0123 <2405606352@qq.com>
2023-05-29 16:07:11 +0800

    'update'

--- 爬取.bat
+++ 爬取.bat
@@ -1,1 +1,20 @@
-start cmd /c "python douban.py&&git add *&&git commit -m '更新'&&git push"
+﻿@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM Companyname: 
+REM Productname: 
+REM Filedescription: 
+REM Copyrights: 
+REM Trademarks: 
+REM Originalname: 
+REM Comments: 
+REM Productversion:  0. 0. 0. 0
+REM Fileversion:  0. 0. 0. 0
+REM Internalname: 
+REM ExeType: console
+REM Architecture: x64
+REM Appicon: 
+REM AdministratorManifest: No
+REM  QBFC Project Options End
+@ECHO ON
+start cmd /c "python douban.py&&git add *&&git commit -m 'update'&&git push"
