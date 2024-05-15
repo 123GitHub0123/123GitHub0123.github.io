4a682a47fc8794d8d839a2adbfb8eb75074fed7e
123GitHub0123 <89198225+123GitHub0123@users.noreply.github.com>
2024-05-15 19:19:02 +0800

    Update douban.py

--- douban.py
+++ douban.py
@@ -33,8 +33,7 @@ file_obj.write("""
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
     <style>
         /* 自定义样式，使链接看起来像普通文本 */
         .info-link {
